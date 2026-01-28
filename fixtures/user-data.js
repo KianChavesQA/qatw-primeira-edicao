5e8dc50dda8dd2e6efc6e6b9a073df2d6896278b
Kian Chaves <kianchaves@live.com>
2026-01-28 01:48:02 -0300

    feat: initial playwright setup and enviromnment config

---
+++ fixtures/user-data.js
@@ -0,0 +1,5 @@
+export const user = {
+  cpf: "00000014141",
+  password: "147258",
+  invalid2faCode: "123456",
+};
---
+++ tests/login.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+import { user } from "../fixtures/user-data.js";
+
+test("shouldn't login with invalid 2fa authenticator code", async ({
+  page,
+}) => {
+  await page.goto("http://paybank-mf-auth:3000/");
+  await page.getByRole("textbox", { name: "Digite seu CPF" }).fill(user.cpf);
+  await page.getByRole("button", { name: "Continuar" }).click();
+  for (const char of user.password) {
+    await page.getByRole("button", { name: char }).click();
+  }
+  await page.getByRole("button", { name: "Continuar" }).click();
+  await page.getByRole("textbox", { name: "000000" }).fill(user.invalid2faCode);
+  await page.getByRole("button", { name: "Verificar" }).click();
+  await expect(page.locator("span")).toContainText(
+    "Código inválido. Por favor, tente novamente.",
+  );
+});
